08ee8a4269c73e0e815ce6eb5e6c1ffb9feaa71e
sandip124 <sandip.choudhary@crystalsolution.com.np>
2020-04-02 13:05:41 +0545

    initial commit

---
+++ ViewComponents/EmptyState.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EmptyState.ViewComponents
+{
+    [ViewComponent(Name = "EmptyState")]
+    public class EmptyState : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(EmptyStateViewModel emptyStateViewModel)
+        {
+            return await Task.Run(() => View(emptyStateViewModel));
+        }
+    }
+}
---
+++ ViewComponents/EmptyStateViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmptyState.ViewComponents
+{
+    public class EmptyStateViewModel
+    {
+        public string message { get; set; } = "Result is empty.";
+        public string controllerName { get; set; }
+        public string actionName { get; set; } = "create";
+        public string buttonClass { get; set; } = "btn btn-primary btn-sm";
+        public string buttonIcon { get; set; } = "fa fa-plus fa-md";
+        public string buttonText { get; set; } = "Add";
+    }
+}
